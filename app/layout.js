a317c4c224e8b0c43426d72e471d9496ea9dc53d
anmol5050 <anmolraj55055@gmail.com>
2026-05-16 14:32:07 +0530

    Update footer

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by Anmol & Ayush</p>
+                <p>Made with 💗 by Anmol             </p>
               </div>
             </footer>
           </ThemeProvider>
